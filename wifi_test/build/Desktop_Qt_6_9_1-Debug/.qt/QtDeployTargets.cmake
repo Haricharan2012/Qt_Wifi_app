17285dde2176db3424520f8f32a7dd0e95ea8338
Haricharan <Haricharan43019@gmail.com>
2025-10-09 21:34:25 +0530

    Thu Oct  9 09:34:25 PM IST 2025

--- wifi_test/build/Desktop_Qt_6_9_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_wifi_test_FILE /home/warrior-of-solitude/Documents/linux_app_dev/wifi_test/build/Desktop_Qt_6_9_1-Debug/wifi_test)
-set(__QT_DEPLOY_TARGET_wifi_test_TYPE EXECUTABLE)
