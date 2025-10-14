418ebec5e800b687eae5c54d3f1fda4776deca39
Haricharan <Haricharan43019@gmail.com>
2025-10-14 19:05:01 +0530

    Tue Oct 14 07:05:01 PM IST 2025

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -29,7 +29,7 @@ void MainWindow::on_pushButton_2_clicked()
 
     QProcess proc1;
 
-    FILE * flpt;
+    FILE * flpt;  //
 
 
 
@@ -47,9 +47,9 @@ void MainWindow::on_pushButton_2_clicked()
 
 
 
-    flpt = fopen("wifi.sh", "w+");   // open a file in  reading mode?
+    flpt = fopen("wifi.sh", "w+");   // open a file in  writing mode
 
-    system("chmod u+x wifi.sh");
+    system("chmod u+x wifi.sh");  //making sh file executable
 
     if(flpt==NULL)
     {
@@ -59,16 +59,10 @@ void MainWindow::on_pushButton_2_clicked()
     else
     {
 
-     //   printf("\n enter the wifi ssid");
-       // scanf("%s",ssid);
-
-       // printf("\n enter the wifi password");
-       // scanf("%s",pass);
-
         qDebug() << "\n file opened";
 
-        /* fputs(texdata,flpt);  // put data inside file where the pointer is pointing to
-      fputs("\n",flpt);   */
+
+   //adding text in file
 
         fprintf(flpt,"#!/usr/bin/bash\n");
         fprintf(flpt,"echo \"Enabling wifi connection\" \n ");
@@ -78,7 +72,7 @@ void MainWindow::on_pushButton_2_clicked()
         fclose(flpt);
         printf("\ndata is written successfully");
         printf("\nfile is now closed");
-        system("./wifi.sh");
+        system("./wifi.sh");                  //executing the sh file
     }
 
 }
@@ -86,73 +80,6 @@ void MainWindow::on_pushButton_2_clicked()
 
 
 
-  /* // proc1.start("nmcli", QStringList()<<"dev wifi connect" <<ssix << "password" << passix ");
-
-    QStringList args;
-    char outxx[100];
-
-
-    args <<"dev" << "wifi" << "connect" <<ssix << "password" << passix;
-
-    proc1.start("nmcli",args);
-
-    proc1.waitForFinished();
-
-
-
-   // proc1.start("nmcli device status");
-   // proc1.waitForFinished();
-
-     QString out=proc1.readAllStandardError();    //reports only error and read allstandard output report only succes output
-
-    // out=proc1.readAllStandardOutput();
-
-     std::string outst  = out.toStdString();
-
-     const char* outx=outst.c_str();
-
-     sprintf(outxx,"%s",outx);
-
-     //cout<<endl<<out.toStdString();
-     QMessageBox::information(this, "wifi-notify" ,outxx);  */
-
-
- // try your best
-
-
-   //  proc1.start("gedit",QStringList()<<"go.txt");
-
-    // proc1.waitForFinished();
-
-
-    //system("gedit");
-
-
-
-
-   /* qDebug("dodo");
-    qDebug("%s",passtx);
-
-   // sprintf(temp,"%s",ssid);
-   // sprintf(sim,"%s",pass);
-
-  //you are my friend
-
-    //char ssid[10] = ui->lineEdit->text();
-
-
-    char fincmd[100];
-
-   QMessageBox::information(this, "wifi-notify" , "ing connection");
-
-
-   //-----------------string crt --------------------------------//
-
-   sprintf(fincmd,"nmcli dev wifi connect '%s' password '%s'",ssix,passix);
-
-   QMessageBox::information(this, "wifi-notifyx" ,fincmd);
-
-    system(fincmd); */
 
 
 void MainWindow::on_pushButton_clicked()
---
+++ readme.txt
@@ -0,0 +1,1 @@
+this is a wifi implementation using file handling c-version!
